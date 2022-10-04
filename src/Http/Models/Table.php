cfab34990c194d1bd7588abe4d0bd8b0fff4d14c
noap <noa.peguin@hotmail.com>
2022-10-04 16:47:02 +0200

    getValidationRules for Table model

--- src/Http/Models/Table.php
+++ src/Http/Models/Table.php
@@ -16,6 +16,8 @@ class Table extends BasicShell
 
     /**
      * Define hasMany relation with Group model only the Group have parent_is_group to false.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function groups()
     {
@@ -24,9 +26,25 @@ class Table extends BasicShell
 
     /**
      * Has many relation with Data model only the Data have parent_is_group to false.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function datas()
     {
         return $this->hasMany(Data::class, 'parent_id')->where('parent_is_group', false);
+    }
+
+    /**
+     * Get validation rules, return an array with the rules without dynamic calling.
+     * 
+     * @return array
+     */
+    public function getValidationRules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'render' => 'nullable|string',
+        ];
     }
 }
