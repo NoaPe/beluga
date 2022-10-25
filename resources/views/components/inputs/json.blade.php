41027b7640b7c2bb71776ad9d0c6a085ff0f33be
NoaPe <noa.peguin@hotmail.com>
2022-10-25 14:59:39 +0200

    Bugfix : json input

--- resources/views/components/inputs/json.blade.php
+++ resources/views/components/inputs/json.blade.php
@@ -3,7 +3,7 @@
     id="{{ $name }}"
     rows="5"
     placeholder="{{ isset($data->placeholder) ? $data->placeholder : '' }}"
-    {{  isset($data->settings->required)  &&  ? $data->settings->required : '' }}
+    {{  isset($data->settings->required) ? $data->settings->required : '' }}
 >
 {{ old($name) ?? $shell->getAttribute($name) ?? $data->default ?? '' }}
 </textarea>
